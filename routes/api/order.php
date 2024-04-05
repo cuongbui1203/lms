0806ead324d26e063b26f1eae18fdc1096262db1
Kuon <cuongbuimanh1203@gmail.com>
2024-04-05 21:28:50 +0700

    cap nhat chuc nang xac nhan da den cho don hang. ho tro xu ly dong thoi nhieu don hang 1 luc #29

--- routes/api/order.php
+++ routes/api/order.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/', [OrderController::class, 'store']);
 Route::get('/multi/next', [OrderController::class, 'getNextPos']);
 Route::post('/multi/next', [OrderController::class, 'moveToNextPos']);
-Route::put('/{order}/arrived', [OrderController::class, 'arrivedPos']);
+Route::put('/multi/arrived', [OrderController::class, 'arrivedPos']);
 Route::get('/{order}', [OrderController::class, 'show']);
 Route::delete('/{order}', [OrderController::class, 'destroy']);
 Route::post('/{order}', [OrderController::class, 'addDetail']);
